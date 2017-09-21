025b8e5af7ba4cf05590e31ddf50eadfc77be5ad
rflukes <flukes.2@osu.edu>
2017-09-21 18:33:48 -0400

    implemented getRecipes with query

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ import atexit
 import cf_deployment_tracker
 import os
 import json
+import sys
+import watson_developer_cloud
+from watson_developer_cloud import DiscoveryV1
 
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
@@ -17,17 +20,16 @@ db = None
 
 #takes in a list of ingredients, returns list of possible recipes
 def getRecipes(ingredientsList):
-
-	recipeList = []	
-	recipe1 = {'title':'Toast Sandwich', 'ingredients':[], 'description':'', 'instructions':[], 'imageUrl':'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/An_image_of_a_toast_sandwich%2C_shot_from_the_side.jpg/440px-An_image_of_a_toast_sandwich%2C_shot_from_the_side.jpg'}
-	
-	#TODO logic
-	
-	recipeList.append(recipe1);
-	recipeList.append(recipe1);
-
-
-	return recipeList
+    discovery = DiscoveryV1(    
+        username='ff7c4fbe-e752-4c91-b99e-7f7db797e294',
+        password='E3qe7yeI5Nrf',
+        version='2017-09-01'
+    )
+    ingred_str = ','.join([i for i in ingredientsList])
+    query_str = "Ingredients:" + ingred_str
+    qopts = {'query': query_str}
+    my_query = discovery.query('0a15c836-8ec9-41ca-a33b-93a9d63dae8d', '7844f79c-c259-4a3d-a2d8-2db7d18acd76', qopts)
+    return my_query['results']
 
 
 
