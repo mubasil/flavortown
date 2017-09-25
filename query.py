7a4d08ed9316d41e74b9966086454440f46d4f3d
rflukes <flukes.2@osu.edu>
2017-09-24 23:42:02 -0400

    queries all combinations of recipes

--- query.py
+++ query.py
@@ -2,6 +2,7 @@ import sys
 import os
 import json
 import watson_developer_cloud
+import itertools
 from watson_developer_cloud import DiscoveryV1
 
 
@@ -15,13 +16,24 @@ class Discovery(object):
 
     def query(self, ingreds):
         query_str = "Ingredients:" + ingred_str
-        qopts = {'query': query_str}
+        qopts = {'query': query_str, 'filter': query_str}
         return self.discovery.query('0a15c836-8ec9-41ca-a33b-93a9d63dae8d', '7844f79c-c259-4a3d-a2d8-2db7d18acd76', qopts)
 
-
-ingredients = ['vanilla extract', 'milk', 'coconut', 'sugar']
-ingred_str = ','.join([i for i in ingredients])
 discovery = Discovery()
-my_query = discovery.query(ingred_str)
-print(json.dumps(my_query, indent=2))
+ingredients = ['garlic powder', 'paprika', 'popped popcorn', 'watermelon']
+x = len(ingredients)
+while x > 0:
+  combo = itertools.combinations(ingredients,x)
+  x -= 1
+  recipe_found = False
+  for c in combo:
+    ingred_str = ','.join([word for word in c])
+    my_query = discovery.query(ingred_str)
+    if my_query['matching_results'] > 0:
+      print(json.dumps(my_query, indent=2))
+      recipe_found = True
+  if recipe_found:
+    break
+
+
 
