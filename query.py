d08ba3cc38e56f68501bd7a49695cfb0fc4e7fbd
rflukes <flukes.2@osu.edu>
2017-09-25 21:49:58 -0400

    implemented logic for near Recipe matches

--- query.py
+++ query.py
@@ -15,13 +15,14 @@ class Discovery(object):
           version='2017-09-01'
         )
 
-    def query(self, ingredients):
+    def query(self, ingredients, exact=True):
         ingred_str = '|'.join([word for word in ingredients])
         query_str = "Ingredients:" + ingred_str
         qopts = {'query': query_str, 'count':1000}
         my_query = self.discovery.query('0a15c836-8ec9-41ca-a33b-93a9d63dae8d', 
             '7844f79c-c259-4a3d-a2d8-2db7d18acd76', qopts)
-        return self.processResponse(my_query['results'])   
+        if exact: return self.processExactMatches(my_query['results'])
+        else: return self.processNearMatches(my_query['results'])
              
     #c:candidate ingredient list
     #p:provided ingredient list
@@ -29,23 +30,40 @@ class Discovery(object):
         misses = 0
         for ingredient in c:
             match = process.extractOne(ingredient, p)
-            if match[1] < 80: misses = misses + 1
+            if match[1] < 90: misses = misses + 1
             if misses > 0: return False
         return True
 
+    def isNearMember(self, c, p):
+        misses = 0
+        for ingredient in c:
+            match = process.extractOne(ingredient, p)
+            if match[1] < 90: misses = misses + 1
+            if misses > 1: return False
+        if misses == 1: return True
+        else: return False
+
     #take the response from Watson and only keep recipes that are a good match
-    def processResponse(self, results):
+    def processExactMatches(self, results):
         trimmed_results = []
         for recipe in results:
             lst = recipe['Ingredients']
             if self.isMember(lst, ingredients): trimmed_results.append(recipe)
         return trimmed_results
 
+    #take the response from Watson and only keep recipes that one ingredient from being a perfect match
+    def processNearMatches(self, results):
+        trimmed_results = []
+        for recipe in results:
+            lst = recipe['Ingredients']
+            if self.isNearMember(lst, ingredients): trimmed_results.append(recipe)
+        return trimmed_results
+
 
 discovery = Discovery()
 ingredients =['apple', 'celery', 'walnuts', 'raisins', 'mayonnaise','onion', 
-    'vegetable oil', 'paprika', 'mushrooms', 'chicken broth', 'sausage']
-my_query = discovery.query(ingredients) 
+    'olive oil', 'vegetable oil', 'paprika', 'mushrooms', 'chicken broth', 'sausage', 'chicken breast', 'avocado']
+my_query = discovery.query(ingredients, False) 
 print(json.dumps(my_query, indent=2))
 
 
