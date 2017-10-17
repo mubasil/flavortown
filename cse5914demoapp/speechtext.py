9d12654be319c38739aea84e9b4e4162d2f4c4e9
Ian <irpennin@gmail.com>
2017-10-17 19:21:19 +0000

    indent fix

--- cse5914demoapp/speechtext.py
+++ cse5914demoapp/speechtext.py
@@ -9,12 +9,12 @@ from watson_developer_cloud import TextToSpeechV1 as TextToSpeech
 class SpeechText(object):
     def __init__(self):      
         self.STT = SpeechToText(
-          username='824801e9-c8af-4ecd-ac9a-bbc001cf7769',
-          password='2Ug6krjqdA8R'
+            username='824801e9-c8af-4ecd-ac9a-bbc001cf7769',
+            password='2Ug6krjqdA8R'
         )
-		self.TTS = TextToSpeech(
-          username='159ac011-2033-457d-bd66-f5bddc650685',
-          password='aVtZSYPCAe8L'
+        self.TTS = TextToSpeech(
+            username='159ac011-2033-457d-bd66-f5bddc650685',
+            password='aVtZSYPCAe8L'
         )
 
     def transcribe_audio(audio_file):
