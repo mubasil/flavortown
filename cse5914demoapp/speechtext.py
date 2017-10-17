1e623d94d8252aea3ca56aa850325c75952b1ad6
mubasil <mubasil@yahoo.com>
2017-10-17 16:40:46 -0400

    fixed stuff

--- cse5914demoapp/speechtext.py
+++ cse5914demoapp/speechtext.py
@@ -8,17 +8,11 @@ from watson_developer_cloud import TextToSpeechV1 as TextToSpeech
 
 class SpeechText(object):
     def __init__(self):      
-        self.STT = SpeechToText(
-          username='824801e9-c8af-4ecd-ac9a-bbc001cf7769',
-          password='2Ug6krjqdA8R'
-        )
-		self.TTS = TextToSpeech(
-          username='159ac011-2033-457d-bd66-f5bddc650685',
-          password='aVtZSYPCAe8L'
-        )
+		self.STT = SpeechToText(username='824801e9-c8af-4ecd-ac9a-bbc001cf7769', password='2Ug6krjqdA8R')
+		self.TTS = TextToSpeech(username='159ac011-2033-457d-bd66-f5bddc650685', password='aVtZSYPCAe8L')
 
-    def transcribe_audio(audio_file):
-        return STT.recognize(audio_file, content_type=’audio/wav’)
+    def transcribe_audio(self, audio_file):
+        return self.STT.recognize(audio_file, content_type='audio/wav')['results'][0]['alternatives'][0]['transcript']
 
-    def speak_text(text):
-		return TTS.recognize(audio_file, content_type=’audio/wav’)
+    def speak_text(self, text):
+		return self.TTS.synthesize(text, accept='audio/wav')
